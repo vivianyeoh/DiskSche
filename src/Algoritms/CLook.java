7d982bc2305a24e63fa6ebff342ab88b139478f7
vivian.yeoh <khcy6yhw@nottingham.edu.my>
2016-12-07 11:13:27 +0800

    update clook

--- src/Algoritms/CLook.java
+++ src/Algoritms/CLook.java
@@ -20,17 +20,36 @@ public class CLook extends ScheAlgorithm {
 	public void arrangeList() {
 		Collections.sort(reqList);
 
+		//position of headStart and position of max head start is different in a way
+		//for example: when we have a headstart of 2, and req number of 2, 2, 3, 4
+		//value for positionOfHeadStart is 3 and positionOfMaxHeadStart is 2
+		//however, positionOfMaxHeadStart is important to have because
+		//when we have a headstart of 8 abd reqNumber of 6, 7, 8, 8, 8,9
+		//value for positionOfHeadStart is 9 and positionOfMaxHeadStart is 8
 		int positionOfHeadStart = 0;
+		int positionOfMaxHeadStart=0;
+
+		
 		// find the position of headstart
 		for (int i = 0; i < reqList.size(); i++) {
-			
-			// positionofheadstart is first number bigger than or equal to headstart
-			if (reqList.get(i) >= headStart) {
-				
+			// positionofheadstart is first number bigger than headstart
+			if (reqList.get(i) > headStart) {
 				positionOfHeadStart = i;
 				break;
 			}
 		}
+		
+		for (int i = 0; i < reqList.size(); i++) {
+			// positionofheadstart is first number bigger than or equal to headstart
+			if (reqList.get(i) >= headStart) {
+				
+				positionOfMaxHeadStart = i;
+				break;
+			}
+		}
+		
+		
+		System.out.println("positionOfHeadStart: "+positionOfHeadStart);
 		
 		// let headstart be the first dot in toBeArranged list
 		toBeArranged.add(headStart);
@@ -42,21 +61,23 @@ public class CLook extends ScheAlgorithm {
 			// smallest value
 			// start from first number that is smaller than headstart
 			for (int i = positionOfHeadStart; i >= 0; i--) {
+				System.out.println(" Nearer to zero i: "+i+" reqList.get(i)"+reqList.get(i));
 				toBeArranged.add(reqList.get(i));
 			}
 			// follow by biggest number
 			for (int i = reqList.size() - 1; i >positionOfHeadStart; i--) {
+				System.out.println(" Nearer to zero i: "+i+" reqList.get(i)"+reqList.get(i));
 				toBeArranged.add(reqList.get(i));
 			}
 		} else {
 			// Nearer to end, direction is to end, smallest value after larger
 			// value
 			// start from first number that is bigger than or equal to headstart
-			for (int i = positionOfHeadStart; i < reqList.size(); i++) {
+			for (int i = positionOfMaxHeadStart; i < reqList.size(); i++) {
 				toBeArranged.add(reqList.get(i));
 			}
 			// follow by 0 and following number
-			for (int i = 0; i < positionOfHeadStart; i++) {
+			for (int i = 0; i < positionOfMaxHeadStart; i++) {
 				toBeArranged.add(reqList.get(i));
 			}
 		}
@@ -67,14 +88,10 @@ public class CLook extends ScheAlgorithm {
 	public int getTtlHeadMovement() {
 		int total = 0;
 		// not comparing headstart
-		int maxIndex = 1, minIndex;
+		int maxIndex=1, minIndex=1;
 
 		// get Max and min value
-		for (int i = 1; i < getArrangedList().size(); i++) {
-			if (getArrangedList().get(i) > getArrangedList().get(maxIndex)) {
-				maxIndex = i;
-			}
-		}
+		
 
 		// direction to start
 		// Nearer to zero, direction is to start, largest value after smallest
@@ -82,7 +99,12 @@ public class CLook extends ScheAlgorithm {
 		// skip the addition between maxIndex and minIndex because huge jump
 		// doesn't count as a head movement in CLook
 		if (toStart) {
-			minIndex = maxIndex - 1;
+			for (int i = 1; i < getArrangedList().size(); i++) {
+				if (getArrangedList().get(i) <= getArrangedList().get(minIndex)) {
+					minIndex = i;
+				}
+			}
+			maxIndex = minIndex + 1;
 			System.out.println("maxIndex" + maxIndex);
 			System.out.println("minIndex" + minIndex);
 			for (int i = 0; i < minIndex; i++) {
@@ -101,6 +123,11 @@ public class CLook extends ScheAlgorithm {
 			// direction to end
 			// Nearer to end, direction is to end, smallest value after larger
 			// value
+			for (int i = 1; i < getArrangedList().size(); i++) {
+				if (getArrangedList().get(i) >= getArrangedList().get(maxIndex)) {
+					maxIndex = i;
+				}
+			}
 			minIndex = maxIndex + 1;
 			System.out.println("maxIndex" + maxIndex);
 			System.out.println("minIndex" + minIndex);
